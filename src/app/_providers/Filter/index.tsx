04c9ef1005b8574850375b68f9f653c44ce0c3cb
Iván <ivanlitt8@gmail.com>
2024-03-14 03:11:04 -0300

    user screens have been modified, and categories have been added

---
+++ src/app/_providers/Filter/index.tsx
@@ -0,0 +1,25 @@
+// 'use client'
+
+// import { ReactNode, useState } from 'react';
+
+// export const INITIAL_FILTER_DATA = {
+//   categoryFilters: [],
+//   setCategoryFilters: () => [],
+//   sort: '',
+//   setSort: () => '',
+// }
+
+// const FilterContext = createContext(INITIAL_FILTER_DATA);
+
+// export const FilterProvider = ({ children }: {children: React.ReactNode}) => {
+
+//     const [categoryFilters, setCategoryFilters]useState([])
+
+//     return (
+//         <FilterContext.Provider value={(
+
+//         )}>
+//             {children}
+//         </FilterContext.Provider>
+//     )
+// }
